21d0a1ed5c6a7e1769cae0e68e8e83346620c864
kodeu <koueni-anael.deumeni@polymtl.ca>
2017-07-24 13:39:38 -0400

    Initial release.

---
+++ app/src/main/java/com/example/surface/roadbudee/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.surface.roadbudee;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.VideoView;
+import android.widget.MediaController;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.citytopview));
+        videoView.start();
+    }
+}
